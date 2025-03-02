8f5783316b47e180ded1a0d678de50a7b6cb4ebe
DSprout <sprout.daniel@outlook.com>
2025-03-02 14:35:51 -0600

    predictive code test

--- test_for_space4_prediction.ino
+++ test_for_space4_prediction.ino
@@ -31,13 +31,14 @@ void setup()
 {
     // put your setup code here, to run once:
     Servo1.attach(PWMsig);
-    delay(5);
+    delay(100);
     Servo1.write(reset); // right
     servoAngle = reset;
+    delay(100);
     Serial.begin(9600);
 }
 
-#define servoStep 10
+#define servoStep 2
 double convertDegreeToRadians(int angleDegrees){
     return angle * M_PI / 180;
 }
@@ -67,6 +68,16 @@ void loop()
             prevCoordinate.y_coord = 0;
             prevCoordinate.x_coord = distInCM;
             prevTime = millis();
+
+            Serial.print("Angle: ");
+            Serial.println(servoAngle);
+            Serial.print("Time: ");
+            Serial.println(prevTime);
+            Serial.print("Coordinate(x,y): ");
+            Serial.print(prevCoord.x_coord);
+            Serial.print(", ");
+            Serial.println(prevCoord.y_coord);
+
             notFound = false;
         }
     }
@@ -75,18 +86,37 @@ void loop()
         // new servo angle
         servoAngle += servoStep;
         Servo1.write(servoAngle);
+        delay(100);
         
+        int magnitude;
         notFound = true;
         while (notFound)
         {
             distInCM = mySensor.distance();
-            if (distInCM <= 40 && distInCM >= 10)
+            if (distInCM <= 55 && distInCM >= 10)
             {
                 // set initial coordinate
                 newTime = millis();
                 newCoord.x_coord = distInCM * cos(convertDegreeToRadians(servoAngle));
                 newCoord.y_coord = distInCM * sin(convertDegreeToRadians(servoAngle));
-                
+                velo.x_velocity = (newCoord.x_coord - prevCoord.x_coord) / (newTime - prevTime);
+                velo.y_velocity = (newCoord.y_coord - prevCoord.y_coord) / (newTime - prevTime);
+        
+                // prediction
+
+                // time
+                // yposition + Tyvelo / xposition + Txvelo = tan(servoAngle + servoStep)
+                // yposition + Tyvelo = (xposition + Txvelo)tan(servoAngle + servoStep)
+                // Tyvelo - (Txvelo)tan(servoAngle + servoStep) = (xposition)tan(servoAngle + servoStep) - yposition
+                // T(yvelo - (xvelo)tan(servoAngle + servoStep)) = (xposition)tan(servoAngle + servoStep) - yposition
+                // T = ((xposition)tan(servoAngle + servoStep) - yposition) / (yvelo - (xvelo)tan(servoAngle + servoStep))
+                double tangent = tan(convertDegreeToRadians(servoAngle + servoStep));
+                int predict_time = ((newCoord.x_coord * tangent) - newCoord.y_coord) / (velo.y_velocity - (velo.x_velocity * tangent));
+                // location
+                Coordinate predict;
+                predict.x_coord = newCoord.x_coord + (velo.x_velocity * predict_time);
+                predict.y_coord = newCoord.y_coord + (velo.y_velocity * predict_time);
+
                 Serial.print("Angle: ");
                 Serial.println(servoAngle);
                 Serial.print("Time: ");
@@ -94,17 +124,24 @@ void loop()
                 Serial.print("Coordinate(x,y): ");
                 Serial.print(newCoord.x_coord);
                 Serial.print(", ");
-                Serial.println(newCoord.y_coord);
-                Serial.println();
+                Serial.println(newCoord.y_coord);                
+                Serial.println("Prediction(t,x,y): ");
+                Serial.println(newTime + predict_time);
+                Serial.print(", ");
+                Serial.println(predict.x_coord);
+                Serial.print(", ");
+                Serial.println(predict.y_coord);
 
+                magnitude = sqrt((predict.x_coord * predict.x_coord) + (predict.y_coord * predict.y_coord));
                 prevCoord = newCoord;
                 notFound = false;
             }
         }
 
         // end scan and reset if prediction puts next scan out of range
-        if(false){
-
+        if(magnitude > 55){
+            Serial.print("Program Stopped: Object out of range");
+            while(true){}
         }
         /* code */
     }
